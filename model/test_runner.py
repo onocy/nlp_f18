56dc1a094f6d910f1501d89dfa3f19c37962403e
Jesse Bartola <jrbartola@gmail.com>
2018-11-03 16:00:51 -0400

    Organized utility structure and finished computation implementations

--- model/test_runner.py
+++ model/test_runner.py
@@ -1,22 +1,32 @@
 import unittest
 
-from test.util_tests import UtilTests
+from test.comp_tests import ComputationTests
+from test.file_tests import FileTests
 
 
 def generate_test_suite():
     suite = unittest.TestSuite()
 
-    # Add Util tests to suite
-    add_util_tests(suite)
+    # Add Computation tests to suite
+    add_computation_tests(suite)
+
+    # Add File I/O tests to suite
+    add_file_io_tests(suite)
 
     return suite
 
 
-def add_util_tests(suite):
-    suite.addTest(UtilTests('test_compute_pca'))
-    suite.addTest(UtilTests('test_compute_song_vector'))
-    suite.addTest(UtilTests('test_compute_artist_vector'))
-    suite.addTest(UtilTests('test_tokenize_csv'))
+def add_computation_tests(suite):
+    suite.addTest(ComputationTests('test_compute_pca'))
+    suite.addTest(ComputationTests('test_compute_song_vector'))
+    suite.addTest(ComputationTests('test_compute_artist_vector'))
+    suite.addTest(ComputationTests('test_lyrics_to_word_matrix'))
+
+
+def add_file_io_tests(suite):
+    suite.addTest(FileTests('test_tokenize_csv'))
+    suite.addTest(FileTests('test_load_word_embeddings'))
+
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
---
+++ model/util/io_helper.py
@@ -0,0 +1,66 @@
+"""File I/O utilities for tokenizing CSV data and loading word embeddings.
+"""
+
+import numpy as np
+
+
+def tokenize_csv(f, artist_col, song_col, lyric_col):
+    """
+    Tokenizes a csv file containing (artist -> lyric) mappings
+    Args:
+        f (str): The name of the csv file to be tokenized
+        artist_col (int): A 0-indexed integer denoting the column in the csv corresponding to artist names
+        song_col (int): A 0-indexed integer denoting the column in the csv corresponding to song names
+        lyric_col (int): A 0-indexed integer denoting the column in the csv corresponding to lyric strings
+    Returns:
+        dict[str, dict[str], list[str]]]: A dictionary mapping artist names to a dictionary mapping song names to a list
+          of words in each song
+    """
+
+    import nltk
+    import csv
+    import re
+
+    # TODO: To save time, avoid performing this check if punkt is already downloaded.
+    nltk.download('punkt')
+
+    artists = {}
+    with open(f) as file:
+        file_csv = csv.reader(file, quotechar='"')
+
+        for row in file_csv:
+            artist = row[artist_col]
+            song = row[song_col]
+            lyrics = nltk.word_tokenize(re.sub('\'|,|\(|\)|\?|\!', '', row[lyric_col].lower()))
+            if artist not in artists:
+                artists[artist] = {}
+            artists[artist][song] = lyrics
+
+    return artists
+
+
+def load_word_embeddings(f, unzip=False):
+    """
+    Loads word embeddings from a specified file
+    Args:
+        f (str): The path to a file containing word embeddings
+    Optional:
+        unzip (bool): True if the file specified is a .zip, False by default
+    Returns:
+        dict[str, np.ndarray]: A mapping from words to their embedding vectors
+    """
+    if unzip:
+        import zipfile
+        vec_file = zipfile.ZipFile(f, 'r').open(f[:-4], 'r')
+    else:
+        vec_file = open(f, 'r')
+
+    vocab = {}
+    for line in vec_file:
+        split = line.strip().split()
+        word = split[0].lower()
+        if word not in vocab:
+            vocab[word] = np.zeros((1, len(split) - 1))
+        vocab[word] = np.array(split[1:], dtype='float32')
+
+    return vocab
