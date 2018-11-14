cb7ff5e6490b3cfd34aa9af5b5085653c43b0b37
Jesse Bartola <jrbartola@gmail.com>
2018-11-13 23:33:01 -0500

    Network is runnable with artist or genre classification

--- model/rnn.py
+++ model/rnn.py
@@ -15,8 +15,10 @@ class ArtistLSTM(nn.Module):
         Initializes our ArtistNet LSTM network
         Params:
             embedding_dim (int): The number of dimensions in each of the word embedding vectors
-            output_size (int): The number of distinct artists that the network will run a prediction over
+            hidden_dim (int): The number of dimensions in each hidden unit
+            output_size (int): The number of output classes that the network will run a prediction over
             vocab_size (int): The number of distinct words in our vocabulary
+            max_input_len (int): The fixed-size of the input layer to the network
         """
         super(ArtistLSTM, self).__init__()
 
@@ -94,6 +96,8 @@ class ArtistLSTM(nn.Module):
             opt_algo (func): The optimizer algorithm to use. Defaults to torch.optim.Adam
         """
 
+        import time
+
         # Declare our loss functions and optimizers
         loss = loss_fn()
         optimizer = opt_algo(self.parameters(), lr=learning_rate)
@@ -101,12 +105,14 @@ class ArtistLSTM(nn.Module):
         # Loop over every epoch
         for ep in range(num_epochs):
             ep_loss = 0.
+            start_time = time.time()
 
-            # Shuffle the training data before selecting out batches
+            # Shuffle the training data before selecting our batches
             random.shuffle(training_data)
 
             # Loop over each batch
             for start in range(0, len(training_data), batch_size):
+                # print("Batch start: {}".format(start))
 
                 # Clear the hidden states from the previous batch
                 self.hidden = self.__init_hidden()
@@ -132,8 +138,8 @@ class ArtistLSTM(nn.Module):
                 batch_loss.backward()  # Performs our back-propagation by computing gradients
                 optimizer.step()       # Updates our parameters using the calculated gradients
 
-            # Print out our loss at the end of every epoch
-            print("Epoch #{}: {}".format(ep, ep_loss))
+            # Print out our loss and elapsed time at the end of every epoch
+            print("Epoch #{}: {} ({} minutes)".format(ep, ep_loss, (time.time() - start_time) / 60))
 
     def predict(self, lyrics, vocab):
         """
@@ -148,41 +154,25 @@ class ArtistLSTM(nn.Module):
         # Get the artist with the highest probability
         return torch.argmax(self(input_tensor)).item()
 
-    def test_batch(self, test_data):
-        corr = 0.
-        total = 0.
-
-        for start in range(0, len(test_data), 32):
-            batch = test_data[start:start+1]
-            in_mat = torch.zeros(len(batch), self.max_input_length, dtype=torch.long)
-            truths = torch.zeros(len(batch), dtype=torch.long)
-
-            for i, (artist, lyrics) in enumerate(batch):
-                truths[i] = artist
-                for j, lyric in enumerate(lyrics[:self.max_input_length]):
-                    in_mat[i, j] = lyric
-
-            preds = net(in_mat)
-            max_pred = torch.argmax(preds)
-            if max_pred == artist:
-                corr += 1
-            total += 1
-
-        print(corr, total, corr / total)
-
-    def test(self, test_data):
+    def test(self, test_data, k=1):
         """
         Tests the network on the given test data
         Args:
             test_data (list[(int, np.ndarray)]): A list of test tuples of the form (artist_idx, word_embeddings)
+        Optional:
+            k (int): The number of nearest neighbors to calculate the prediction results for. By default, k=1
         """
         num_correct = 0
         for artist, lyrics in test_data:
+
+            # Construct the fixed-length input vector and fill it up
             input_vec = torch.zeros(1, self.max_input_length, dtype=torch.long)
             for i in range(min(self.max_input_length, len(lyrics))):
                 input_vec[0][i] = lyrics[i]
-            prediction = torch.argmax(self(input_vec)).item()
-            if prediction == artist:
+
+            # Grab the k-highest prediction indices and check if the actual artist is one of them
+            predictions = self(input_vec).detach().numpy().argsort()[0][::-1][:k]
+            if artist in predictions:
                 num_correct += 1
 
         print("Number correct: {}".format(num_correct))
@@ -190,32 +180,36 @@ class ArtistLSTM(nn.Module):
         print("Test accuracy: {:.2f}%".format(num_correct / len(test_data) * 100))
 
 
+def run_artist_net(train=True):
+    """
+    Runs our LSTM RNN on the artist dataset
+    Optional:
+        train (bool): If True, re-trains the network. If False, only the tests are run. An exception will be raised
+          if there is no pickled network object in the current directory
+    """
 
-if __name__ == '__main__':
-    import os
-    if os.path.isfile('learner.pickle') and sys.argv[1] is None:
-        print('Reading from pickle file...')
-        learner = unpickle_object('genre.pickle')
+
+def run_genre_net(train=True):
+    """
+    Runs our LSTM RNN on the genre dataset
+    Optional:
+        train (bool): If True, re-trains the network. If False, only the tests are run. An exception will be raised
+          if there is no pickled network object in the current directory
+    """
+
+    # Import the genre dictionary if it exists, otherwise create it from the lyrics CSV
+    if os.path.isfile('genre.pickle'):
+        genre_dict = unpickle_object('genre.pickle')
+        print("Unpickled the genre dictionary.")
     else:
         genre_dict = tokenize_csv_pandas('../lyrics.csv')
-        pickle_object(genre_dict, 'genre.pickle')
+        print("Tokenized the genre dictionary from the csv file.")
 
-    # Load our word embeddings and artist dictionary
-    # vocab = load_word_embeddings('../glove.6B.50d.txt')
-    # artist_dict = tokenize_csv('../songdata.csv', 0, 1, 3)
-    # pickle_object(artist_dict, 'artists.pickle')
-    artist_dict = unpickle_object('artists.pickle')
-    vocab_index = create_vocab_index(artist_dict)
+    vocab_index = create_vocab_index_from_genre(genre_dict)
+    genre_indices = create_genre_index(genre_dict)
+    print("Created vocab and genre indices.")
 
-    # Filter our artist dictionary down to just 5 artists
-    artist_dict = {artist: artist_dict[artist] for artist in random.sample(artist_dict.keys(), 5)}
-    print("Chosen artists: ", artist_dict.keys())
-
-    artist_indices = create_artist_index(artist_dict)
-
-    # Our input data
-    input_data = build_input_data(artist_dict, vocab_index, artist_indices)
-    print("Built input data.")
+    input_data = build_genre_input_data(genre_dict, vocab_index, genre_indices)
 
     # Shuffle our input data and split it into 20% test, 80% training data
     random.shuffle(input_data)
@@ -224,12 +218,77 @@ if __name__ == '__main__':
     training_data = input_data[:training_length]
     test_data = input_data[training_length:]
 
-    # Create our neural network with 50-count word embeddings
-    net = ArtistLSTM(100, 10, len(artist_dict), len(vocab_index), max_input_len=300)
-    # net = unpickle_object('net.pickle')
-    print("About to train the network!")
+    # If the training argument was set to True, train a newly constructed network using the following hyperparameters
+    if train:
+        EMBEDDING_DIM = 100
+        HIDDEN_DIM = 8
+        OUTPUT_SIZE = len(genre_dict)
+        VOCAB_SIZE = len(vocab_index)
+        MAX_INPUT_LEN = 300
 
-    # Training time!
-    net.train_network(training_data, batch_size=64, num_epochs=20, learning_rate=0.09)
-    net.test(test_data)
-    pickle_object(net, 'net.pickle')
+        net = ArtistLSTM(EMBEDDING_DIM, HIDDEN_DIM, OUTPUT_SIZE, VOCAB_SIZE, MAX_INPUT_LEN)
+
+        print("Training network...")
+        net.train_network(training_data, batch_size=64, num_epochs=20, learning_rate=0.005)
+    else:
+        net = unpickle_object('rnn.pickle') # NOTE: This will throw an error if there is no pickle!
+
+    # Test the trained network with k closest predicted neighbors
+    print("Testing the network...")
+    net.test(test_data, k=2)
+
+
+def run_artist_net(train=True):
+    """
+    Runs our LSTM RNN on the artist dataset
+    Optional:
+        train (bool): If True, re-trains the network. If False, only the tests are run. An exception will be raised
+          if there is no pickled network object in the current directory
+    """
+
+    # Import the artist dictionary if it exists, otherwise create it from the lyrics CSV
+    if os.path.isfile('artists.pickle'):
+        artist_dict = unpickle_object('artists.pickle')
+        print("Unpickled the artist dictionary.")
+    else:
+        artist_dict = tokenize_csv('../songdata.csv')
+        print("Tokenized the artist dictionary from the csv file.")
+
+    vocab_index = create_vocab_index(artist_dict)
+    artist_indices = create_artist_index(artist_dict)
+    print("Created vocab and artist indices.")
+
+    input_data = build_input_data(artist_dict, vocab_index, artist_indices)
+
+    # Shuffle our input data and split it into 20% test, 80% training data
+    random.shuffle(input_data)
+    training_length = int(0.8 * len(input_data))
+
+    training_data = input_data[:training_length]
+    test_data = input_data[training_length:]
+
+    # If the training argument was set to True, train a newly constructed network using the following hyperparameters
+    if train:
+        EMBEDDING_DIM = 100
+        HIDDEN_DIM = 8
+        OUTPUT_SIZE = len(artist_dict)
+        VOCAB_SIZE = len(vocab_index)
+        MAX_INPUT_LEN = 300
+
+        net = ArtistLSTM(EMBEDDING_DIM, HIDDEN_DIM, OUTPUT_SIZE, VOCAB_SIZE, MAX_INPUT_LEN)
+
+        print("Training network...")
+        net.train_network(training_data, batch_size=64, num_epochs=20, learning_rate=0.005)
+    else:
+        net = unpickle_object('rnn.pickle') # NOTE: This will throw an error if there is no pickle!
+
+    # Test the trained network with k closest predicted neighbors
+    print("Testing the network...")
+    net.test(test_data, k=2)
+
+
+if __name__ == '__main__':
+    import os.path
+
+    run_artist_net()
+    # run_genre_net()
